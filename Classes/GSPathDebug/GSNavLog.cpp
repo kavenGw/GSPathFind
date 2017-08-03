70a421e99ffbfabfec04bfcda7645ac03a557116
Can Wu <371905989@qq.com>
2017-08-04 07:57:36 +0800

    log输出

--- Classes/GSPathDebug/GSNavLog.cpp
+++ Classes/GSPathDebug/GSNavLog.cpp
@@ -15,22 +15,45 @@ GSNavLog::GSNavLog()
 
 void GSNavLog::addInfoLog(const char *format, ...)
 {
+    va_list argp;
+    va_start(argp, format);
     
+    printf(format,argp);
+    
+    va_end(argp);
 }
 
 void GSNavLog::addWarningLog(const char *format, ...)
 {
     
+    va_list argp;
+    va_start(argp, format);
+    
+    printf(format,argp);
+    
+    va_end(argp);
 }
 
 void GSNavLog::addErrorLog(const char *format, ...)
 {
     
+    va_list argp;
+    va_start(argp, format);
+    
+    printf(format,argp);
+    
+    va_end(argp);
 }
 
 void GSNavLog::addLog(gsNavLogCategory category, const char *format, ...)
 {
-
+    
+    va_list argp;
+    va_start(argp, format);
+    
+    printf(format,argp);
+    
+    va_end(argp);
 }
 
 void GSNavLog::changeLogState(gsNavLogState state)
